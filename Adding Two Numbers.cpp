7648a9a999870d86d73d62275f6810ee7f639579
V. Bala Karthik <vasamsettibalakarthik7@gmail.com>
2025-12-02 10:14:42 +0530

    Add files via upload

---
+++ Adding Two Numbers.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Reading two integers from input
+    scanf("%d %d", &num1, &num2);
+
+    // Printing their sum
+    printf("%d", num1 + num2);
+
+    return 0;
+}
+
